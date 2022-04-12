917fd93db387d94c1e04e2a8326235fb99b8512d
Maryam Naveed <63284481+mareyam@users.noreply.github.com>
2022-04-12 23:14:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,98 +1,99 @@
+
+
 $(function() {
   loadRec();
-  $("#recipes").on("click",".btn-danger",handleDelete);
-  $("#recipes").on("click",".btn-warning",handleUpdate);
-  console.log("1");
-
-  // $(".addBtn").click(addProduct);
+  $("#products").on("click",".btn-danger",deleteRec);
+  $("#products").on("click",".btn-warning",updateProduct);
+  
+  $("#addBtn").click(addProduct);
   $("#updateSave").click(function() {
-    var id = $("#updateId").val();
-    var name = $("#updateName").val();
-    var price = $("#updatePrice").val();
-    $.ajax({
-        url:"https://usman-recipes.herokuapp.com/api/products/"+id,
-        data:{name, price},
-        method: "PUT",
-        success: function(response)
-        {
-          console.log(response);
-          loadRec();
-          $("#updateModal").modal("hide ");
-        }
-    });
-  });
+         var id = $("#updateId").val();
+         var name = $("#updateName").val();
+         var price = $("#updatePrice").val();
+         $.ajax({
+             url:"https://usman-recipes.herokuapp.com/api/products/"+id,
+             data:{name, price},
+             method: "PUT",
+             success: function(response)
+             {
+               console.log(response);
+               loadRec();
+               $("#updateModal").modal("show");
+             }
+         });
+});
 });
 
-function handleUpdate() {
-
-  var btn = $(this);
-  var parentDiv = btn.closest(".recipesJSClass");
-  var id = parentDiv.attr("data-id");
-  
- $.get("https://usman-recipes.herokuapp.com/api/products/"+id,function(response) {
-  $("#updateId").val(response._id);
-  $("#updateTitle").val(response.name);
-  $("#updateBody").val(response.price);
-  $("#updateModal").modal("show");
-  });
-}
-
-function handleDelete() {
-  console.log("2");
-  var btn = $(this);
-  var parentDiv = btn.closest(".recipesJSClass");
-  var id = parentDiv.attr("data-id");
-  console.log("id is "+id);
+function loadRec() {
+  console.log("hello");
   $.ajax({
-    url: "https://usman-recipes.herokuapp.com/api/products/"+id,
-    method:"DELETE",
-    success: function() {
-      console.log("3");
+    url:"https://usman-recipes.herokuapp.com/api/products/",
+    method: "GET",
+    success: function(response) {
+      console.log("loaded");
+      var productID = $("#products");
+      productID.empty();
+
+      for(var i=0; i<response.length;i++)
+      {
+        var responseResult = response[i];
+        productID.append(
+             `<div class="productsJSClass" data-id="${responseResult._id}"
+                     <h4>${responseResult.name}
+                     <button class="btn btn-danger delete float-right">Delete</button>
+                     <button class="btn btn-warning info float-right">Edit</button>
+                     </h4>
+                     <p>${responseResult.price}</p>
+                     </div>`
+        )
+      }
+    }
+  })
+}
+function deleteRec() {
+  var btn = $(this);
+  var parentDiv = btn.closest(".productsJSClass");
+  var id = parentDiv.attr("data-id");
+  console.log(id);
+
+  $.ajax({
+    url:"https://usman-recipes.herokuapp.com/api/products/"+id,
+    method: "DELETE",
+    success: function(response) {
+      console.log("deleted");
       loadRec();
     }
-  });
-  console.log("4");
-}
-function loadRec() {
-  $.ajax({
-    url: "https://usman-recipes.herokuapp.com/api/products/",
-    method:"GET",
-    success: function(response) {
-        
-        var result = $("#recipes");
-        result.empty();
-        for(var i=0; i<response.length; i++)
-        {
-          var rec = response[i];
-          result.append( `<div class="recipesJSClass" data-id="${rec._id}"
-          <h4>${rec.name}
-          <button class="btn btn-danger delete float-right">Delete</button>
-          <button class="btn btn-warning info float-right">Edit</button>
-          </h4>
-          <p>${rec.price}</p>
-          </div>
-          `)
-        }
-    }
-  });
+  })
 }
 function addProduct() {
   var name = $("#name").val();
   var price = $("#price").val();
   
-console.log("going to product");
   $.ajax({
-    url: "https://usman-recipes.herokuapp.com/api/products/",
-    method:"POST",
-    data:{name:name,price:price},
-    success:function(response)
-    {
+    url:"https://usman-recipes.herokuapp.com/api/products/",
+    method: "POST",
+    data:{name:name, price:price},
+    success: function(response) {
+      console.log("added");
       console.log(response);
-      console.log("going to product 2");
       loadRec();
     }
+  })
+}
+function updateProduct() {
+  console.log("in update");
+  var btn = $(this);
+  var parentDiv = btn.closest(".productsJSClass");
+  var id = parentDiv.attr("data-id");
+  console.log(id);
+  $.get("https://usman-recipes.herokuapp.com/api/products/"+id,function(response) {
+  $("#updateId").val(response._id);
+  $("#updateName").val(response.name);
+  $("#updatePrice").val(response.price);
+  $("#updateModal").modal("show");
+  loadRec();
   });
-  console.log("done to product");
+
 }
 
 
@@ -124,21 +125,96 @@ console.log("going to product");
 
 
 
-//recent 9/4 115 pm
 
+
+//working 11.04 10 15pm
 // $(function() {
 //   loadRec();
 //   $("#recipes").on("click",".btn-danger",handleDelete);
-//   $("#recipes").on("click",".btn-warning",handleDelete);
+//   $("#recipes").on("click",".btn-warning",handleUpdate);
+//   console.log("1");
 
+//   $(".addBtn").click(addProduct);
+//   $("#updateSave").click(function() {
+//     var id = $("#updateId").val();
+//     var name = $("#updateName").val();
+//     var price = $("#updatePrice").val();
+//     $.ajax({
+//         url:"https://usman-recipes.herokuapp.com/api/products/"+id,
+//         data:{name, price},
+//         method: "PUT",
+//         success: function(response)
+//         {
+//           console.log(response);
+//           loadRec();
+//           $("#updateModal").modal("show");
+//         }
+//     });
+//   });
 // });
-// function handleUpdate() {
-//   $("#updateModal").modal("show");
+
+// function loadRec() {
+//   $.ajax({
+//     url: "https://usman-recipes.herokuapp.com/api/products/",
+//     method:"GET",
+//     success: function(response) {
+        
+//         var result = $("#recipes");
+//         result.empty();
+//         for(var i=0; i<response.length; i++)
+//         {
+//           var rec = response[i];
+//           result.append( `<div class="recipesJSClass" data-id="${rec._id}"
+//           <h4>${rec.name}
+//           <button class="btn btn-danger delete float-right">Delete</button>
+//           <button class="btn btn-warning info float-right">Edit</button>
+//           </h4>
+//           <p>${rec.price}</p>
+//           </div>
+//           `)
+//         }
+//     }
+//   });
 // }
-// function addRecipe()
-// {
+
+// function handleDelete() {
+//   console.log("2");
+//   var btn = $(this);
+//   var parentDiv = btn.closest(".recipesJSClass");
+//   var id = parentDiv.attr("data-id");
+//   console.log("id is "+id);
+//   $.ajax({
+//     url: "https://usman-recipes.herokuapp.com/api/products/"+id,
+//     method:"DELETE",
+//     success: function() {
+//       console.log("3");
+//       loadRec();
+//     }
+//   });
+//   console.log("4");
+// }
+
+
+// function handleUpdate() {
+
+//   var btn = $(this);
+//   var parentDiv = btn.closest(".recipesJSClass");
+//   var id = parentDiv.attr("data-id");
+  
+//  $.get("https://usman-recipes.herokuapp.com/api/products/"+id,function(response) {
+//   $("#updateId").val(response._id);
+//   $("#updateName").val(response.name);
+//   $("#updatePrice").val(response.price);
+//   $("#updateModal").modal("show");
+//   });
+// }
+
+
+// function addProduct() {
 //   var name = $("#name").val();
 //   var price = $("#price").val();
+  
+// console.log("going to product");
 //   $.ajax({
 //     url: "https://usman-recipes.herokuapp.com/api/products/",
 //     method:"POST",
@@ -146,55 +222,36 @@ console.log("going to product");
 //     success:function(response)
 //     {
 //       console.log(response);
+//       console.log("going to product 2");
 //       loadRec();
 //     }
-//   })
-
-// }
-// function handleDelete() {
-//  var btn = $(this);
-//  var parentDiv = btn.closest(".recipes");
-//  let id = parentDiv.attr("data-id");
-//  console.log("id is"+id);
-//  console.log("deleted");
-//  $.ajax({
-//     url: "https://usman-recipes.herokuapp.com/api/products/"+id,
-//     method:"DELETE",
-//     success: function(response)
-//     {
-//       loadRec();
-//     }
-//  });
-//  console.log("dleete");
+//   });
+//   console.log("done to product");
 // }
 
 
 
-// function loadRec() {
-//   $.ajax({
-//     url: "https://usman-recipes.herokuapp.com/api/products",
-//     method:"GET",
-//     success: function(response){
-//       var result = $("#recipes");
-//       result.empty();
-//       for(var i=0;i<response.length;i++)
-//       {
-        
-//         var rec = response[i];
-//         result.append(`<div class="recipes" data-id="${rec._id}">
-//         <h3>
-//         ${rec.name}
-//         </h3>
-//         <p>
-//         <button class="btn btn-danger float-right">Delete</button>
-//         <button class="btn btn-info float-right">Edit</button>
-//         ${rec.price}
-//         </p>
-//         </div>`)
-//       }
-//     }
-//   })
-// }
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
